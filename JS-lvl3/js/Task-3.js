4a1b8ee1c5e002e4dffada91cef767847f662124
YuraGoloborodko <mysoapfordeal@gmail.com>
2020-10-15 22:07:10 +0300

    Done

---
+++ JS-lvl3/js/Task-3.js
@@ -0,0 +1,38 @@
+const findBestEmployee = function(employees) {
+    const entries = Object.entries(employees)
+    let moreTask = 0;
+    for(const entry of entries){
+      if(entry[1] > moreTask){
+        moreTask = entry[1];
+      }
+    }
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(
+    findBestEmployee({
+      ann: 29,
+      david: 35,
+      helen: 1,
+      lorence: 99,
+    }),
+  ); // lorence
+  
+  console.log(
+    findBestEmployee({
+      poly: 12,
+      mango: 17,
+      ajax: 4,
+    }),
+  ); // mango
+  
+  console.log(
+    findBestEmployee({
+      lux: 147,
+      david: 21,
+      kiwi: 19,
+      chelsy: 38,
+    }),
+  ); // lux
